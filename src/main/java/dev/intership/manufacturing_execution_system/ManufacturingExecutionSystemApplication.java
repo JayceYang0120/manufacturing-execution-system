c9ae81061a8528376137fecaf5dcbdbcd088adfb
atzaa <atzaa@DESKTOP-RUS0S9A>
2026-05-29 13:35:32 +0800

    initializing project

---
+++ src/main/java/dev/intership/manufacturing_execution_system/ManufacturingExecutionSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.intership.manufacturing_execution_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturingExecutionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturingExecutionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/intership/manufacturing_execution_system/ManufacturingExecutionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.intership.manufacturing_execution_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufacturingExecutionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
